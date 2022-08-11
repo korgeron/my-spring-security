0f0c2fb3c2e65fa90ba9cf66d38abac789fa1ccc
orgeron.kevin <orgeron.kevin@gmail.com>
2022-08-11 16:40:54 -0500

    push

--- src/main/java/com/security/myspringsecurity/config/SecurityConfig.java
+++ src/main/java/com/security/myspringsecurity/config/SecurityConfig.java
@@ -2,16 +2,33 @@ package com.security.myspringsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
 
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+       return http.authorizeRequests()
+               .antMatchers("/").permitAll()
+               .and()
+               .authorizeRequests()
+               .antMatchers("/hello").hasAnyAuthority("USER")
+               .and().formLogin()
+               .and()
+               .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
